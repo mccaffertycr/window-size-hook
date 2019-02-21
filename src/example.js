9985d4c881478f05a16bdbec4fef20f70d44be54
mccaffertycr <mccaffertycr@gmail.com>
2019-02-21 18:13:51 -0500

    additional props

--- src/example.js
+++ src/example.js
@@ -2,14 +2,14 @@ import React from "react";
 import { useWindowSize } from "./windowsize";
 
 function App() {
-  let width = useWindowSize();
+  let size = useWindowSize();
   const breakpoints = [768, 991, 1199];
   const getDevice = size => {
-    if (size <= breakpoints[0]) return "mobile";
-    else if (size <= breakpoints[1]) return "tablet";
+    if (size.innerWidth <= breakpoints[0]) return "mobile";
+    else if (size.innerWidth <= breakpoints[1]) return "tablet";
     else return "desktop";
   };
-  return <div className="App">{getDevice(width)}</div>;
+  return <div className="App">{getDevice(size)}</div>;
 }
 
 export default App;
--- src/windowsize.js
+++ src/windowsize.js
@@ -1,8 +1,17 @@
 import { useState, useEffect } from "react";
 
+const getWindowSize = () => {
+  return {
+    innerHeight: window.innerHeight,
+    innerWidth: window.innerWidth,
+    outerHeight: window.outerHeight,
+    outerWidth: window.outerWidth
+  };
+};
+
 export function useWindowSize() {
-  const [windowSize, setWindowSize] = useState(window.innerWidth);
-  const handleResize = () => setWindowSize(window.innerWidth);
+  const [windowSize, setWindowSize] = useState(getWindowSize());
+  const handleResize = () => setWindowSize(getWindowSize());
 
   useEffect(() => {
     window.addEventListener("resize", handleResize);
